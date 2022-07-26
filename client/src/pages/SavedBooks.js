807674b4b969cdfc6b162599dd55e3dcb357a0b2
Brandon <brandonperez@brandons-mbp.lan>
2022-07-26 18:06:13 -0400

    updated front-end api calls, moving on to update pages/components files with updated mutations and queries

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 
 import { getMe, deleteBook } from '../utils/API';
+import { GET_ME, DELETE_BOOK } from '../utils/mutation';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
---
+++ client/src/utils/mutation.js
@@ -0,0 +1,81 @@
+import { gql } from '@apollo/client';
+
+export const GET_ME = gql`
+{
+    me{
+        _id
+        username
+        email
+        savedBooks {
+            _id
+            authors
+            description
+            bookId
+            image
+            link
+            title
+        }
+    }
+}`
+
+export const CREATE_USER = gql`
+mutation addUser($username: String!, $email: String!, $password: String!){
+    addUser(username:$username, email: $email, password: $password){
+        token
+        user{
+            _id
+            username
+        }
+    }
+}`
+
+export const LOGIN_USER = gql`
+mutation login($email: String!, $password: String!){
+    login(email: $email, password: $password){
+        token
+        user{
+            _id
+            username
+        }
+    }
+}`
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($bookData: String!){
+        saveBook(bookData: $bookData){
+            _id
+            authors
+            description
+            bookId
+            image
+            link
+            title
+            user {
+                _id
+                username
+            }
+        }
+    }`
+
+    export const DELETE_BOOK = gql`
+    mutation deleteBook($bookData: String!){
+        deleteBook(bookId: $bookId){
+            _id
+            authors
+            description
+            bookId
+            image
+            link
+            title
+            user {
+                _id
+                username
+            }
+        }
+    }`
+
+// make a search to google books api
+// https://www.googleapis.com/books/v1/volumes?q=harry+potter
+export const searchGoogleBooks = (query) => {
+    return fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
+};
